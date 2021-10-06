7362fba8b09f17381cf7ea4dc657a0f0b8f6958e
clink10Zero <47035153+clink10Zero@users.noreply.github.com>
2021-10-06 09:00:44 +0200

    c'est bon...

---
+++ 24 carrot pirate/Assets/Map/Scripts/Filtre.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Filtre
+{
+    public enum TypeFiltre { centre, bord };
+    public AnimationCurve moditication;
+    public TypeFiltre type;
+
+    public float[,] MakeFiltre(int mapWidth, int mapHeight)
+    {
+
+        return null;
+    }
+
+}
