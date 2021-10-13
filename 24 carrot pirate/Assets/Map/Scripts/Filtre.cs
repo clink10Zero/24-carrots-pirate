ba7b44ab17e94f2df3fa2624e2b6d1d942278000
clink10Zero <47035153+clink10Zero@users.noreply.github.com>
2021-10-13 10:02:50 +0200

    map avance
    tout tout tout vous sur les maps

--- 24 carrot pirate/Assets/Map/Scripts/Filtre.cs
+++ 24 carrot pirate/Assets/Map/Scripts/Filtre.cs
@@ -2,16 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Filtre
+public static class Filtre
 {
-    public enum TypeFiltre { centre, bord };
-    public AnimationCurve moditication;
-    public TypeFiltre type;
 
-    public float[,] MakeFiltre(int mapWidth, int mapHeight)
+    public static float[,] MakeFiltre(FiltreStruct filtre, int mapWidth, int mapHeight)
     {
+        float[,] filtreMap = new float[mapWidth, mapHeight];
 
-        return null;
+        switch(filtre.type)
+        {
+            case FiltreStruct.TypeFiltre.bord :
+                filtreMap = MakeBord(filtre.moditication, mapWidth, mapHeight);
+                break;
+            case FiltreStruct.TypeFiltre.centre :
+                filtreMap = MakeCentre(filtre.moditication, mapWidth, mapHeight);
+                break;
+
+        }
+
+        return filtreMap;
+    }
+
+    private static float[,] MakeBord(AnimationCurve curve, int mapWidth, int mapHeight)
+    {
+        float[,] filtreMap = new float[mapWidth, mapHeight];
+        for(int y = 0; y < mapHeight/2; y++)
+        {
+            for(int x = 0; x < mapWidth/2; x++)
+            {
+                float fx = (float)x / (float)mapWidth;
+                float fy = (float)y / (float)mapHeight;
+                
+                float xv = curve.Evaluate(fx);
+                float yv = curve.Evaluate(fy);
+
+                filtreMap[x, y] = xv * yv;
+                filtreMap[x, mapHeight - y - 1] = xv * yv;
+                filtreMap[mapWidth - x - 1, y] = xv * yv;
+                filtreMap[mapWidth - x - 1, mapHeight - y - 1] = xv * yv;
+            }
+        }
+        return filtreMap;
+    }
+
+    private static float[,] MakeCentre(AnimationCurve curve, int mapWidth, int mapHeight)
+    {
+        float[,] filtreMap = new float[mapWidth, mapHeight];
+        for (int y = 0; y < mapHeight / 2; y++)
+        {
+            for (int x = 0; x < mapWidth / 2; x++)
+            {
+                float fx = (float)x / (float)mapWidth;
+                float fy = (float)y / (float)mapHeight;
+                float xv = curve.Evaluate(fx);
+                float yv = curve.Evaluate(fy);
+                if(yv > xv)
+                {
+                    filtreMap[x, y] = yv;
+                    filtreMap[x, mapHeight - y - 1] = yv;
+                    filtreMap[mapWidth - x - 1, y] = yv;
+                    filtreMap[mapWidth - x - 1, mapHeight - y - 1] = yv;
+                }
+                else if (xv > yv)
+                {
+                    filtreMap[x, y] = xv;
+                    filtreMap[x, mapHeight - y - 1] = xv;
+                    filtreMap[mapWidth - x - 1, y] = xv;
+                    filtreMap[mapWidth - x - 1, mapHeight - y - 1] = xv;
+                }
+                else
+                {
+                    filtreMap[x, y] = xv;
+                    filtreMap[x, mapHeight - y - 1] = xv;
+                    filtreMap[mapWidth - x - 1, y] = xv;
+                    filtreMap[mapWidth - x - 1, mapHeight - y - 1] = xv;
+                }
+            }
+        }
+        return filtreMap;
+    }
+
+    public static float[,] Apply(float[,] noiseMap, float[,] filtre, int mapWidth, int mapHeight)
+    {
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x ++)
+            {
+                noiseMap[x, y] *= filtre[x, y];
+            }
+        }
+
+        return noiseMap;
     }
 
 }
